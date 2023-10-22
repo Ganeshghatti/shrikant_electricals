2c76bcd7030e734aa3549f4a21c2158f3a02879c
Ganesh <ghattiganesh8@gmail.com>
2023-10-22 20:44:28 +0530

    mew

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-// const express = require("express");
-// const connectdatabase = require("./config/database");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const customerroutes = require("./routes/Customer");
-// const employeeroutes = require("./routes/Employee");
-
-// require("dotenv").config();
-
-// const app = express();
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(cors());
-// app.use(customerroutes);
-// app.use(employeeroutes);
-
-// connectdatabase();
-// const PORT = process.env.PORT;
-
-// const cloudinary = require("cloudinary").v2;
-
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARYNAME,
-//   api_key: process.env.CLOUDINARYAPIKEY,
-//   api_secret: process.env.CLOUDINARYAPISECRET,
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
-
-const express = require('express')
-
-const app = express()
-
-require('dotenv').config()
-
-app.use(express.json())
-
+const express = require("express");
 const connectdatabase = require("./config/database");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const customerroutes = require("./routes/Customer");
+const employeeroutes = require("./routes/Employee");
 
-connectdatabase()
+require("dotenv").config();
 
-const PORT = process.env.PORT
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(customerroutes);
+app.use(employeeroutes);
+
+connectdatabase();
+const PORT = process.env.PORT;
+
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARYNAME,
+  api_key: process.env.CLOUDINARYAPIKEY,
+  api_secret: process.env.CLOUDINARYAPISECRET,
+});
 
 app.listen(PORT, () => {
-    console.log("Server is running on port " + PORT)
-})
+  console.log(`Server running at http://localhost:${PORT}`);
+});
