c32ca81888b639b75d74dac92e6d98ffb80e76ca
Ganesh <ghattiganesh8@gmail.com>
2023-10-22 20:22:06 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require("express");
 const connectdatabase = require("./config/database");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const customerroutes=require("./routes/Customer");
+const customerroutes = require("./routes/Customer");
 const employeeroutes = require("./routes/Employee");
 
 require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
-app.use(customerroutes)
+app.use(customerroutes);
 // app.use(employeeroutes);
 
 connectdatabase();
