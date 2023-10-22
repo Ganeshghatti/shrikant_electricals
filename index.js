e02496f19a396eb78abe6c43faf15e10e0d4982e
Ganesh <ghattiganesh8@gmail.com>
2023-10-22 20:28:09 +0530

    Your commit message here

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const connectdatabase = require("./config/database");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const customerroutes = require("./routes/Customer");
-const employeeroutes = require("./routes/Employee");
+// const express = require("express");
+// const connectdatabase = require("./config/database");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const customerroutes = require("./routes/Customer");
+// const employeeroutes = require("./routes/Employee");
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const app = express();
-app.use(express.json());
+// const app = express();
+// app.use(express.json());
 // app.use(bodyParser.json());
 // app.use(cors());
 // app.use(customerroutes);
 // app.use(employeeroutes);
 
-connectdatabase();
-const PORT = process.env.PORT;
+// connectdatabase();
+// const PORT = process.env.PORT;
 
 // const cloudinary = require("cloudinary").v2;
 
@@ -25,6 +25,24 @@ const PORT = process.env.PORT;
 //   api_secret: process.env.CLOUDINARYAPISECRET,
 // });
 
+// app.listen(PORT, () => {
+//   console.log(`Server running at http://localhost:${PORT}`);
+// });
+
+const express = require('express')
+
+const app = express()
+
+require('dotenv').config()
+
+app.use(express.json())
+
+const connectdatabase = require("./config/database");
+
+connectdatabase()
+
+const PORT = process.env.PORT
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+    console.log("Server is running on port " + PORT)
+})
